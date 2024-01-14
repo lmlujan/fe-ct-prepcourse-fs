6a9efccf727fd691e2c732f4b021ab8eee288e36
lmlujan <luismlujan@gmail.com>
2024-01-14 20:25:33 -0300

    Homework mod 06 Bucles

--- M06 Bucles/ejercicios/21.js
+++ M06 Bucles/ejercicios/21.js
@@ -1,9 +1,16 @@
-function esPotenciaDeDos(numero) {
-  // La función recibe un "numero" por argumento.
-  // Determina si es una potencia de 2.
-  // Devuelve true si lo es, sino devuelve false.
-  // PISTA: Utiliza un bucle while.
-  // Tu código:
-}
+  function esPotenciaDeDos(numero) {
+    // La función recibe un "numero" por argumento.
+    // Determina si es una potencia de 2.
+    // Devuelve true si lo es, sino devuelve false.
+    // PISTA: Utiliza un bucle while.
+    // Tu código:
+    while (numero>1){
+      if (numero % 2 !== 0){
+        return false;
+      }
+      numero = numero/2;
+    }
+    return numero === 1;
+  }
 
 module.exports = esPotenciaDeDos;
